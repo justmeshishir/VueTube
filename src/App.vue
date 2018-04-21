2236918603ec174cafb09f1419553a19aa6a6047
Shishir Thapa <shishirthapa1@gamil.com>
2018-04-21 11:07:47 +0545

    VueTube COmplete

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img style="width:140px; margin:14px;" src="./assets/logo.png" />  
+    <VideoPlayer />
+  </div>
+</template>
+
+<script>
+import VideoPlayer from './components/VideoPlayer'
+
+export default {
+  name: 'App',
+  components: {
+    VideoPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
